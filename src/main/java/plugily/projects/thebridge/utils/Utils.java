03c6ddbde60f0574bd3ce0a7030ec3df891afd4c
Tigerpanzer_02 <37453987+Tigerpanzer02@users.noreply.github.com>
2020-11-10 20:08:39 +0100

    Added needed libraries + full thoughts about arena finished

--- src/main/java/plugily/projects/thebridge/utils/Utils.java
+++ src/main/java/plugily/projects/thebridge/utils/Utils.java
@@ -1,5 +1,26 @@
 package plugily.projects.thebridge.utils;
 
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.block.Block;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.SkullMeta;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.util.Vector;
+import pl.plajerlair.commonsbox.minecraft.compat.ServerVersion;
+import pl.plajerlair.commonsbox.string.StringFormatUtils;
+import plugily.projects.thebridge.Main;
+import plugily.projects.thebridge.arena.ArenaRegistry;
+import plugily.projects.thebridge.arena.ArenaState;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -9,6 +30,130 @@ import java.util.regex.Pattern;
  * Created at 31.10.2020
  */
 public class Utils {
+
+  private static Main plugin;
+
+  private Utils() {
+  }
+
+  public static void init(Main plugin) {
+    Utils.plugin = plugin;
+  }
+
+  /**
+   * Serialize int to use it in Inventories size
+   * ex. you have 38 kits and it will serialize it to 45 (9*5)
+   * because it is valid inventory size
+   * next ex. you have 55 items and it will serialize it to 63 (9*7) not 54 because it's too less
+   *
+   * @param i integer to serialize
+   * @return serialized number
+   */
+  public static int serializeInt(Integer i) {
+    if (i == 0) return 9; //The function bellow doesn't work if i == 0, so return 9 in case that happens.
+    return (i % 9) == 0 ? i : (i + 9 - 1) / 9 * 9;
+  }
+
+  /**
+   * Checks whether itemstack is named (not null, has meta and display name)
+   *
+   * @param stack item stack to check
+   * @return true if named, false otherwise
+   */
+  public static boolean isNamed(ItemStack stack) {
+    return stack != null && stack.hasItemMeta() && stack.getItemMeta().hasDisplayName();
+  }
+
+  public static void applyActionBarCooldown(Player p, int seconds) {
+    new BukkitRunnable() {
+      int ticks = 0;
+
+      @Override
+      public void run() {
+        if (!ArenaRegistry.isInArena(p) || ArenaRegistry.getArena(p).getArenaState() != ArenaState.IN_GAME) {
+          this.cancel();
+        }
+        if (ticks >= seconds * 20) {
+          this.cancel();
+        }
+        String progress = StringFormatUtils.getProgressBar(ticks, seconds * 20, 10, "■", ChatColor.COLOR_CHAR + "a", ChatColor.COLOR_CHAR + "c");
+        p.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(plugin.getChatManager().colorMessage("In-Game.Cooldown-Format", p)
+          .replace("%progress%", progress).replace("%time%", String.valueOf((double) ((seconds * 20) - ticks) / 20))));
+        ticks += 10;
+      }
+    }.runTaskTimer(plugin, 0, 10);
+  }
+
+  public static List<Block> getNearbyBlocks(Location location, int radius) {
+    List<Block> blocks = new ArrayList<>();
+    for (int x = location.getBlockX() - radius; x <= location.getBlockX() + radius; x++) {
+      for (int y = location.getBlockY() - radius; y <= location.getBlockY() + radius; y++) {
+        for (int z = location.getBlockZ() - radius; z <= location.getBlockZ() + radius; z++) {
+          blocks.add(location.getWorld().getBlockAt(x, y, z));
+        }
+      }
+    }
+    return blocks;
+  }
+
+  public static Location getBlockCenter(Location location) {
+    return location.clone().add(0.5, 0, 0.5);
+  }
+
+  public static boolean checkIsInGameInstance(Player player) {
+    if (!ArenaRegistry.isInArena(player)) {
+      player.sendMessage(plugin.getChatManager().getPrefix() + plugin.getChatManager().colorMessage("Commands.Not-Playing", player));
+      return false;
+    }
+    return true;
+  }
+
+  public static boolean hasPermission(CommandSender sender, String perm) {
+    if (sender.hasPermission(perm)) {
+      return true;
+    }
+    sender.sendMessage(plugin.getChatManager().getPrefix() + plugin.getChatManager().colorMessage("Commands.No-Permission"));
+    return false;
+  }
+
+  @SuppressWarnings("deprecation")
+  public static SkullMeta setPlayerHead(Player player, SkullMeta meta) {
+    if (Bukkit.getServer().getVersion().contains("Paper") && player.getPlayerProfile().hasTextures()) {
+      return CompletableFuture.supplyAsync(() -> {
+        meta.setPlayerProfile(player.getPlayerProfile());
+        return meta;
+      }).exceptionally(e -> {
+        Debugger.debug(java.util.logging.Level.WARNING, "Retrieving player profile of " + player.getName() + " failed!");
+        return meta;
+      }).join();
+    }
+
+    if (ServerVersion.Version.isCurrentHigher(ServerVersion.Version.v1_12_R1)) {
+      meta.setOwningPlayer(player);
+    } else {
+      meta.setOwner(player.getName());
+    }
+    return meta;
+  }
+
+  public static Vector rotateAroundAxisX(Vector v, double angle) {
+    angle = Math.toRadians(angle);
+    double cos = Math.cos(angle),
+      sin = Math.sin(angle),
+      y = v.getY() * cos - v.getZ() * sin,
+      z = v.getY() * sin + v.getZ() * cos;
+    return v.setY(y).setZ(z);
+  }
+
+  public static Vector rotateAroundAxisY(Vector v, double angle) {
+    angle = -angle;
+    angle = Math.toRadians(angle);
+    double cos = Math.cos(angle),
+      sin = Math.sin(angle),
+      x = v.getX() * cos + v.getZ() * sin,
+      z = v.getX() * -sin + v.getZ() * cos;
+    return v.setX(x).setZ(z);
+  }
 
   public static String matchColorRegex(String s) {
     String regex = "&?#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})";
