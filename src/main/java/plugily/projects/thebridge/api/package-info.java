ad075d66295e3da8e1cd33bf84c6e7ce5c10cfe6
Tigerpanzer_02 <37453987+Tigerpanzer02@users.noreply.github.com>
2020-11-23 13:01:39 +0100

    Added StatsStorage + Updated TB API

--- src/main/java/plugily/projects/thebridge/api/package-info.java
+++ src/main/java/plugily/projects/thebridge/api/package-info.java
@@ -1,5 +1,5 @@
 /**
- * Package with all API events and methods to hook with Murder Mystery.
+ * Package with all API events and methods to hook with The Bridge.
  */
 
 package plugily.projects.thebridge.api;
