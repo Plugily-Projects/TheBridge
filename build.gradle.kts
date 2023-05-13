23801e5ef5fdd8c6a2fb466620de73d023668913
Tigerpanzer_02 <legionofunicorns0202@gmail.com>
2023-05-13 20:16:20 +0200

    Moved gradle to kotlin gradle

---
+++ build.gradle.kts
@@ -0,0 +1,82 @@
+plugins {
+    id("signing")
+    `maven-publish`
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+    java
+}
+
+repositories {
+    mavenLocal()
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    maven("https://oss.sonatype.org/content/repositories/snapshots")
+    maven("https://oss.sonatype.org/content/repositories/central")
+    maven(uri("https://papermc.io/repo/repository/maven-public/"))
+    maven(uri("https://maven.plugily.xyz/releases"))
+    maven(uri("https://maven.plugily.xyz/snapshots"))
+    maven(uri("https://repo.maven.apache.org/maven2/"))
+}
+
+dependencies {
+    implementation("plugily.projects:MiniGamesBox-Classic:1.2.0-SNAPSHOT22") { isTransitive = false }
+    compileOnly("org.spigotmc:spigot-api:1.19.3-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.19.3-R0.1-SNAPSHOT")
+    compileOnly("org.jetbrains:annotations:24.0.1")
+}
+
+group = "plugily.projects"
+version = "1.1.4-SNAPSHOT32"
+description = "TheBridge"
+
+java {
+    withJavadocJar()
+}
+
+tasks {
+    build {
+        dependsOn(shadowJar)
+    }
+
+    shadowJar {
+        archiveClassifier.set("")
+        relocate("plugily.projects.minigamesbox", "plugily.projects.thebridge.minigamesbox")
+        relocate("com.zaxxer.hikari", "plugily.projects.thebridge.database.hikari")
+        minimize()
+    }
+
+    processResources {
+        filesMatching("**/plugin.yml") {
+            expand(project.properties)
+        }
+    }
+
+    javadoc {
+        options.encoding = "UTF-8"
+    }
+
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "Releases"
+            url = uri("https://maven.plugily.xyz/releases")
+            credentials {
+                username = System.getenv("MAVEN_USERNAME")
+                password = System.getenv("MAVEN_PASSWORD")
+            }
+        }
+        maven {
+            name = "Snapshots"
+            url = uri("https://maven.plugily.xyz/snapshots")
+            credentials {
+                username = System.getenv("MAVEN_USERNAME")
+                password = System.getenv("MAVEN_PASSWORD")
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+        }
+    }
+}
