7927e2de8dfa10d780ec06a8dc9e1b0cffef4e88
Lagggpixel <lagggpixel@gmail.com>
2023-06-29 19:23:23 +0800

    LocationSelectorItem -> MultiLocationSelectorItem

--- src/main/java/plugily/projects/thebridge/handlers/setup/SpecificCategory.java
+++ src/main/java/plugily/projects/thebridge/handlers/setup/SpecificCategory.java
@@ -24,6 +24,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.entity.HumanEntity;
 import plugily.projects.minigamesbox.classic.handlers.setup.categories.PluginSpecificCategory;
 import plugily.projects.minigamesbox.classic.handlers.setup.items.category.LocationSelectorItem;
+import plugily.projects.minigamesbox.classic.handlers.setup.items.category.MultiLocationSelectorItem;
 import plugily.projects.minigamesbox.classic.utils.helper.ItemBuilder;
 import plugily.projects.minigamesbox.classic.utils.version.xseries.XMaterial;
 import plugily.projects.minigamesbox.inventory.common.item.ClickableItem;
@@ -41,10 +42,10 @@ public class SpecificCategory extends PluginSpecificCategory {
   public void addItems(NormalFastInv gui) {
     super.addItems(gui);
 
-    LocationSelectorItem bridgeLocation = new LocationSelectorItem(getSetupInventory(), new ItemBuilder(XMaterial.RED_TERRACOTTA.parseMaterial()), "Bridge", "Location where the bridge is.\n Players will be able to break pre-built blocks inside this region.\n its optional", "bridgelocation");
+    MultiLocationSelectorItem bridgeLocation = new MultiLocationSelectorItem(getSetupInventory(), new ItemBuilder(XMaterial.RED_TERRACOTTA.parseMaterial()), "Bridges", "Location of where bridges are. \nPlayers will be able to break pre-built blocks inside this regeion.\nits optional", "bridgelocation", 0);
     gui.setItem((getInventoryLine()*9 + 1), bridgeLocation);
     getItemList().add(bridgeLocation);
-    
+     
     gui.setItem((getInventoryLine() * 9) + 2, ClickableItem.of(new ItemBuilder(XMaterial.ORANGE_STAINED_GLASS_PANE.parseMaterial()).name("&e&lEdit Base").lore(ChatColor.GRAY + "Here you can add/edit a base")
         .lore(ChatColor.GRAY + "Make sure to register the base before continuing!").build(), event -> openBaseMenu(event.getWhoClicked())));
   }
