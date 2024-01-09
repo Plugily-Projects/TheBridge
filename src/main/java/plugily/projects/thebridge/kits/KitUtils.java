e2fc45c25aa23065cfbdbc3826a654eef5105c3d
Wanbin Cao <lagggpixel@gmail.com>
2024-01-09 22:06:44 +0000

    Fixed kits handling items issue
    Took 15 seconds

--- src/main/java/plugily/projects/thebridge/kits/KitUtils.java
+++ src/main/java/plugily/projects/thebridge/kits/KitUtils.java
@@ -16,27 +16,30 @@ import java.util.Optional;
 public class KitUtils {
 
   public static ItemStack handleItem(PluginMain plugin, Player player, ItemStack itemStack) {
-
     Arena arena = (Arena) plugin.getArenaRegistry().getArena(player);
 
     if (arena == null) {
+      plugin.getDebugger().performance("Kit", "Handle item method called for player {1} item stack {2} but the arena was null.", player, itemStack);
       return itemStack;
     }
+    plugin.getDebugger().performance("Kit", "Arena {0} Handle item method called for player {1} item stack {2}.", arena.getId(), player, itemStack);
 
     // Replaces white terracotta with coloured terracotta if the player is in a team
-    if (itemStack.getType().equals(XMaterial.RED_TERRACOTTA) || itemStack.getType().equals(XMaterial.WHITE_TERRACOTTA)) {
+    if (itemStack.getType().equals(XMaterial.RED_TERRACOTTA.parseMaterial()) || itemStack.getType().equals(XMaterial.WHITE_TERRACOTTA.parseMaterial())) {
       Optional<XMaterial> material = XMaterial.matchXMaterial(arena.getBase(player).getMaterialColor().toUpperCase() + "_TERRACOTTA");
       material.ifPresent(xMaterial -> itemStack.setType(Objects.requireNonNull(xMaterial.parseMaterial())));
+      plugin.getDebugger().performance("Kit", "Arena {0} Changing terracotta to {1}.", arena.getId(), arena.getBase(player).getMaterialColor().toUpperCase() + "_TERRACOTTA");
       player.sendMessage("§aYour terracotta has been changed to §6" + arena.getBase(player).getMaterialColor().toUpperCase() + "_TERRACOTTA");
       return itemStack;
     }
 
     // Replaces leather armour with the coloured leather armour if the player is in a team
-    if (itemStack.getType().equals(XMaterial.LEATHER_HELMET) || itemStack.getType().equals(XMaterial.LEATHER_CHESTPLATE) || itemStack.getType().equals(XMaterial.LEATHER_LEGGINGS) || itemStack.getType().equals(XMaterial.LEATHER_BOOTS)) {
+    if (itemStack.getType().equals(XMaterial.LEATHER_HELMET.parseMaterial()) || itemStack.getType().equals(XMaterial.LEATHER_CHESTPLATE.parseMaterial()) || itemStack.getType().equals(XMaterial.LEATHER_LEGGINGS.parseMaterial()) || itemStack.getType().equals(XMaterial.LEATHER_BOOTS.parseMaterial())) {
       LeatherArmorMeta itemMeta = (LeatherArmorMeta) itemStack.getItemMeta();
       assert itemMeta != null;
       itemMeta.setColor(ColorUtil.fromChatColor(ChatColor.valueOf(arena.getBase(player).getColor().toUpperCase())));
       itemStack.setItemMeta(itemMeta);
+      plugin.getDebugger().performance("Kit", "Arena {0} Changing armour to {1}.", arena.getId(), arena.getBase(player).getColor().toUpperCase());
       return itemStack;
     }
 
