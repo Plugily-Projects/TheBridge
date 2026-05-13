ed4ec90505cbbaf04d58fda15ea8c30a9f94cecc
Tigerpanzer_02 <37453987+Tigerpanzer02@users.noreply.github.com>
2026-05-13 23:40:32 +0200

    Fixed block recreation on bridge on legacy versions (colouring)

--- src/main/java/plugily/projects/thebridge/arena/ArenaEvents.java
+++ src/main/java/plugily/projects/thebridge/arena/ArenaEvents.java
@@ -44,6 +44,7 @@ import plugily.projects.minigamesbox.classic.utils.dimensional.Cuboid;
 import plugily.projects.minigamesbox.classic.utils.version.VersionUtils;
 import plugily.projects.minigamesbox.classic.utils.version.events.api.PlugilyEntityPickupItemEvent;
 import plugily.projects.minigamesbox.classic.utils.version.events.api.PlugilyPlayerPickupArrow;
+import plugily.projects.minigamesbox.classic.utils.version.xseries.XBlock;
 import plugily.projects.minigamesbox.classic.utils.version.xseries.XMaterial;
 import plugily.projects.minigamesbox.classic.utils.version.xseries.XSound;
 import plugily.projects.thebridge.Main;
@@ -114,7 +115,7 @@ public class ArenaEvents extends PluginArenaEvents {
       // Only add blocks to the list if the block is not found to be in the broken blocks list
       // Making it so that resetting placed blocks and resetting broken blocks will not tamper with each other
       if(!arena.getBrokenBlocks().containsKey(event.getBlock().getLocation())) {
-        arena.addBrokenBlock(event.getBlock().getLocation(), event.getBlock().getType());
+        arena.addBrokenBlock(event.getBlock().getLocation(), XBlock.getType(event.getBlock()));
       }
     } else {
       new MessageBuilder("IN_GAME_MESSAGES_ARENA_BUILD_BREAK")
